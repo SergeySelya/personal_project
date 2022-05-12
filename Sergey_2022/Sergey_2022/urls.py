318450a82eb945ac956e66321f4f80db98ba70da
sergo <elhom99@gmail.com>
2022-05-12 16:49:32 +0300

    Add app blog

--- Sergey_2022/Sergey_2022/urls.py
+++ Sergey_2022/Sergey_2022/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('info/', include('project.url'))
+    path('', include('project.urls')),
+    path('blog/', include('blog.urls'))
 ]
